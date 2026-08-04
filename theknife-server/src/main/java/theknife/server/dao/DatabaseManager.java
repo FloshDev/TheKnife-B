8d7e4ba3c40426f08dd2d87a56332c564cc3e860
FloshDev <ciani.flavio04@gmail.com>
2026-08-04 17:31:17 +0200

    Creazione classe DatabaseManager

---
+++ theknife-server/src/main/java/theknife/server/dao/DatabaseManager.java
@@ -0,0 +1,43 @@
+package theknife.server.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import theknife.server.ConfigurazioneDB;
+
+/**
+ * Fornisce le connessioni JDBC al database dbTK. Costruisce l'URL a partire
+ * dalla configurazione letta all'avvio e apre una connessione nuova a ogni
+ * richiesta: una Connection non e' thread-safe e i thread del pool operano in
+ * parallelo, quindi non puo' essere condivisa. Chi ottiene la connessione la
+ * chiude, con try-with-resources.
+ *
+ * @author Ciani Flavio Angelo, 761581, VA
+ */
+public class DatabaseManager {
+
+    private final ConfigurazioneDB config;
+
+    public DatabaseManager (ConfigurazioneDB config) {
+        this.config = config;
+    }
+
+    /**
+     * Apre una nuova connessione al database.
+     *
+     * @return una connessione aperta, da chiudere a cura del chiamante
+     * @throws SQLException se le credenziali sono errate, l'host non e'
+     *         raggiungibile o il database non esiste
+     */
+    public Connection getConnection() throws SQLException {
+
+        String url = "jdbc:postgresql://" + config.getHost()
+            + ":" + ConfigurazioneDB.getPortaDb()
+            + "/" + ConfigurazioneDB.getNomeDb();
+
+        return DriverManager.getConnection(url, config.getUtente(), config.getPassword());
+
+    }
+
+}
