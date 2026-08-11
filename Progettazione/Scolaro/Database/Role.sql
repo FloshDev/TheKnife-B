a9f434633eab33bdf9a26e174c2bcc49d0bc48fe
Dev-Scola <gscolaro1@studenti.uninsubria.it>
2026-08-11 11:27:09 +0200

    Update Role.sql

--- Progettazione/Scolaro/Database/Role.sql
+++ Progettazione/Scolaro/Database/Role.sql
@@ -9,6 +9,8 @@ CREATE ROLE tk_app
     LOGIN
     PASSWORD 'TheKnife-B';
 
+CREATE DATABASE "dbTK";
+
 -- Il ruolo riceve i privilegi SOLO sul database dbTK. Concessi in
 -- fase di creazione del db; qui si prepara il contesto applicativo.
-GRANT CONNECT ON DATABASE dbTK TO tk_app;
+GRANT CONNECT ON DATABASE "dbTK" TO tk_app;
