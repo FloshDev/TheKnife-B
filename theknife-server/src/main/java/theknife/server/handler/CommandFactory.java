04e12388dbb9529bc2a7352d057b31a05922d3e8
FloshDev <ciani.flavio04@gmail.com>
2026-08-14 11:23:40 +0200

    CommandFactory su EnumMap invece di un campo per comando

--- theknife-server/src/main/java/theknife/server/handler/CommandFactory.java
+++ theknife-server/src/main/java/theknife/server/handler/CommandFactory.java
@@ -1,25 +1,33 @@
 package theknife.server.handler;
 
+import java.util.EnumMap;
+import java.util.Map;
 import theknife.common.enums.CommandType;
 import theknife.server.command.CercaRistorantiCommand;
 import theknife.server.command.Command;
 import theknife.server.service.RistoranteService;
-
+/**
+ * Costruisce e conserva i Command del server, uno per CommandType. I Command
+ * sono privi di stato, quindi vengono istanziati una sola volta alla partenza
+ * e riusati da tutti i thread client: la mappa e' popolata nel costruttore e
+ * mai piu' modificata.
+ *
+ * @author Ciani Flavio Angelo, 761581, VA
+ */
 public class CommandFactory {
 
-    private final Command cercaRistorantiCommand;
+    private final Map<CommandType, Command> comandi = new EnumMap<>(CommandType.class);
 
     public CommandFactory(RistoranteService ristoranteService) {
-        this.cercaRistorantiCommand = new CercaRistorantiCommand(ristoranteService);
+        comandi.put(CommandType.CERCA_RISTORANTI, new CercaRistorantiCommand(ristoranteService));
+        
     }
 
     public Command create(CommandType commandType) {
-        switch (commandType) {
-            case CERCA_RISTORANTI:
-                return this.cercaRistorantiCommand;
-            // ...
-            default:
-                throw new IllegalArgumentException("Tipo di comando non valido: " + commandType);
+        Command command = comandi.get(commandType);
+        if (command == null) {
+            throw new IllegalArgumentException("Tipo di comando non valido: " + commandType);
         }
+        return command;
     }
 }
