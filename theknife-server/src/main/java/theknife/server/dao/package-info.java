837ee9c46a2f607562592db415b2f0d159746f80
Dev-Scola <gscolaro1@studenti.uninsubria.it>
2026-08-14 13:15:17 +0200

    Aggiunge package-info.java al package server.dao (direttive §5)

---
+++ theknife-server/src/main/java/theknife/server/dao/package-info.java
@@ -0,0 +1,27 @@
+/**
+ * Layer di accesso ai dati (DAO) del server TheKnife.
+ * <p>
+ * Ogni classe incapsula le operazioni sulle tabelle del database
+ * <code>dbTK</code>: apre una connessione tramite
+ * {@link theknife.server.dao.DatabaseManager}, la usa con
+ * <code>try-with-resources</code> e riavvolge le
+ * {@link java.sql.SQLException} in
+ * {@link theknife.server.exception.DataAccessException}. Nessun DAO contiene
+ * logica di dominio: legge e scrive righe, impacchettandole nei DTO del
+ * modulo <code>theknife-common</code>.
+ * <p>
+ * Contratto (sessione 04/08, sezione 4): <code>PreparedStatement</code> per
+ * ogni query, niente stringhe SQL concatenate; le eccezioni SQL non escono
+ * mai da questo package; nessun import dal protocollo
+ * <code>theknife.common.protocol</code>.
+ *
+ * @author Scolaro Gabriele, 760123, VA
+ * @see theknife.server.dao.UtenteDAO
+ * @see theknife.server.dao.ServizioDAO
+ * @see theknife.server.dao.RistoranteDAO
+ * @see theknife.server.dao.RecensioneDAO
+ * @see theknife.server.dao.PreferitoDAO
+ * @see theknife.server.dao.DatabaseManager
+ * @see theknife.server.exception.DataAccessException
+ */
+package theknife.server.dao;
