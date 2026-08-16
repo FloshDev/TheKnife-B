0e1cec2fb355db5799becfa41a12bb1e0a9320f7
Gaspa999 <lgasparini@studenti.uninsubria.it>
2026-08-16 18:36:23 +0200

    Documentazione di RegistrazioneDTO e RispondiRecensioneDTO

--- theknife-common/src/main/java/theknife/common/dto/RegistrazioneDTO.java
+++ theknife-common/src/main/java/theknife/common/dto/RegistrazioneDTO.java
@@ -10,17 +10,64 @@ import theknife.common.enums.Ruolo;
  */
 
 public class RegistrazioneDTO implements Serializable {
+/**
+ * Costante di serializzazione per la compatibilità tra versioni della classe durante la serializzazione e
+ * deserializzazione.
+ */
     private static final long serialVersionUID = 1L;
+    
+/**
+ * Il nome dell'utente che si registra.
+ */
     private String nome;
-     private String cognome;
+    
+/**
+ * Il cognome dell'utente che si registra.
+ */
+    private String cognome;
+    
+/**
+ * Lo username dell'utente che si registra.
+ */
     private String username;
+    
+/**
+ * La password dell'utente che si registra.
+ */
     private String password;
+    
+/**
+ * L'email dell'utente che si registra.
+ */
     private String email;
+    
+/**
+ * Il ruolo dell'utente che si registra.
+ */
     private Ruolo ruolo;
+    
+/**
+ * La data di nascita dell'utente che si registra.
+ */
     private LocalDate dataNascita;
+    
+/**
+ * Il domicilio dell'utente che si registra.
+ */
     private String domicilio;
 
 //Costruttore
+/**
+ * Crea il DTO per la registrazione di un nuovo utente con i dati specificati.
+ * @param nome il nome dell'utente che si registra
+ * @param cognome il cognome dell'utente che si registra
+ * @param username lo username dell'utente che si registra
+ * @param password la password dell'utente che si registra
+ * @param email l'email dell'utente che si registra
+ * @param ruolo il ruolo dell'utente che si registra
+ * @param dataNascita la data di nascita dell'utente che si registra
+ * @param domicilio il domicilio dell'utente che si registra
+ */
     public RegistrazioneDTO(String nome, String cognome, String username, 
         String password, String email, Ruolo ruolo, LocalDate dataNascita, String domicilio) {
         this.nome = nome;
@@ -33,70 +80,140 @@ public class RegistrazioneDTO implements Serializable {
         this.domicilio = domicilio;
     }
 //Setters
-    public void setNome(String x) {
-        this.nome = x;
+/**
+ * Imposta il nome dell'utente che si registra.
+ * @param nome il nome dell'utente
+ */
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public void setCognome(String x) {
-        this.cognome = x;
+/**
+ * Imposta il cognome dell'utente che si registra.
+ * @param cognome il cognome dell'utente
+ */
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
     }
 
-    public void setUsername(String x) {
-        this.username = x;
+/**
+ * Imposta lo username dell'utente che si registra.
+ * @param username lo username dell'utente
+ */
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setPassword(String x) {
-        this.password = x;
+/**
+ * Imposta la password dell'utente che si registra.
+ * @param password la password dell'utente
+ */
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public void setEmail(String x) {
-        this.email = x;
+/**
+ * Imposta l'email dell'utente che si registra.
+ * @param email l'email dell'utente
+ */
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public void setRuolo(Ruolo x) {
-        this.ruolo = x;
+/**
+ * Imposta il ruolo dell'utente che si registra.
+ * @param ruolo il ruolo dell'utente
+ */
+    public void setRuolo(Ruolo ruolo) {
+        this.ruolo = ruolo;
     }
 
-    public void setDataNascita(LocalDate x) {
-        this.dataNascita = x;
+/**
+ * Imposta la data di nascita dell'utente che si registra.
+ * @param dataNascita la data di nascita dell'utente
+ */
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
     }
 
-    public void setDomicilio(String x) {
-        this.domicilio = x;
+/**
+ * Imposta il domicilio dell'utente che si registra.
+ * @param domicilio il domicilio dell'utente
+ */
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
     }
+
 //Getters
+/**
+ * Restituisce il nome dell'utente che si registra.
+ * @return il nome dell'utente
+ */
     public String getNome() {
         return nome;
     }
 
+/**
+ * Restituisce il cognome dell'utente che si registra.
+ * @return il cognome dell'utente
+ */
     public String getCognome() {
         return cognome;
     }
 
+/**
+ * Restituisce lo username dell'utente che si registra.
+ * @return lo username dell'utente
+ */
     public String getUsername() {
         return username;
     }
 
+/**
+ * Restituisce la password dell'utente che si registra.
+ * @return la password dell'utente
+ */
     public String getPassword() {
         return password;
     }
 
+/**
+ * Restituisce l'email dell'utente che si registra.
+ * @return l'email dell'utente
+ */
     public String getEmail() {
         return email;
     }
 
+/**
+ * Restituisce il ruolo dell'utente che si registra.
+ * @return il ruolo dell'utente
+ */
     public Ruolo getRuolo() {
         return ruolo;
     }
 
+/**
+ * Restituisce la data di nascita dell'utente che si registra.
+ * @return la data di nascita dell'utente
+ */
     public LocalDate getDataNascita() {
         return dataNascita;
     }
 
+/**
+ * Restituisce il domicilio dell'utente che si registra.
+ * @return il domicilio dell'utente
+ */
     public String getDomicilio() {
         return domicilio;
     }
+
 //Metodo toString
+/**
+ * Restituisce una rappresentazione testuale dell'oggetto RegistrazioneDTO.
+ * @return la rappresentazione testuale dell'oggetto
+ */
     @Override
     public String toString() {
         return "RegistrazioneDTO [nome=" + nome + ", cognome=" + cognome + ", username=" + username + 
