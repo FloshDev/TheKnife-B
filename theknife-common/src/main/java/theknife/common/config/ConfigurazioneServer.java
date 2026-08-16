540ebfbbc0421e06b5d343e9af0c0515007e6e08
Gaspa999 <lgasparini@studenti.uninsubria.it>
2026-08-16 17:24:28 +0200

    Documentazione di ConfigurazioneServer

--- theknife-common/src/main/java/theknife/common/config/ConfigurazioneServer.java
+++ theknife-common/src/main/java/theknife/common/config/ConfigurazioneServer.java
@@ -9,10 +9,21 @@ package theknife.common.config;
 
 public final class ConfigurazioneServer {
 
+/**
+ * La porta predefinita del server utilizzata per la comunicazione tra client e server.
+ */
     public static final int PORTA_DEFAULT = 9999;
+
+/**
+ * L'host predefinito del server utilizzato dal client per connettersi al server.
+ */
     public static final String HOST_DEFAULT = "localhost";
 
-// Costruttore privato per impedire l'instanziazione
+/**
+ * Costruttore privato per impedire l'instanziazione della classe ConfigurazioneServer.
+ * La classe è progettata per essere utilizzata solo come contenitore di costanti di configurazione e non 
+ * deve essere istanziata.
+ */
     private ConfigurazioneServer() {
         
     }
