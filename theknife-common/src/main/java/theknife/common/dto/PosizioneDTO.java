d688a40b547d2f8d38270f510278ac35cb3d647a
Gaspa999 <lgasparini@studenti.uninsubria.it>
2026-08-16 18:26:07 +0200

    Documentazione di PosizioneDTO e RecensioneDTO

--- theknife-common/src/main/java/theknife/common/dto/PosizioneDTO.java
+++ theknife-common/src/main/java/theknife/common/dto/PosizioneDTO.java
@@ -8,12 +8,35 @@ package theknife.common.dto;
  */
 
 public class PosizioneDTO implements java.io.Serializable {
+/**
+ * Costante di serializzazione per la compatibilità tra versioni della classe durante la serializzazione e 
+ * deserializzazione.
+ */
     private static final long serialVersionUID = 1L;
+    
+/**
+ * La latitudine della posizione geografica.
+ */
     private double latitudine;
+    
+/**
+ * La longitudine della posizione geografica.
+ */
     private double longitudine;
+    
+/**
+ * Il luogo ricavato tramite geocoding dal server.
+ */
     private String luogo;
 
 //Costruttore
+/**
+ * Crea il DTO per la posizione geografica con le coordinate di latitudine e longitudine e il luogo 
+ * specificati.
+ * @param latitudine la latitudine della posizione geografica
+ * @param longitudine la longitudine della posizione geografica
+ * @param luogo il luogo ricavato tramite geocoding dal server
+ */
     public PosizioneDTO(double latitudine, double longitudine, String luogo) {
         this.latitudine = latitudine;
         this.longitudine = longitudine;
@@ -21,31 +44,59 @@ public class PosizioneDTO implements java.io.Serializable {
     }
 
 //Getters e Setters
+/**
+ * Restituisce la latitudine della posizione geografica.
+ * @return la latitudine della posizione geografica
+ */
     public double getLatitudine() {
         return latitudine;
     }
 
+/**
+ * Imposta la latitudine della posizione geografica.
+ * @param latitudine la latitudine della posizione geografica
+ */
     public void setLatitudine(double latitudine) {
         this.latitudine = latitudine;
     }
 
+/**
+ * Restituisce la longitudine della posizione geografica.
+ * @return la longitudine della posizione geografica
+ */
     public double getLongitudine() {
         return longitudine;
     }
 
+/**
+ * Imposta la longitudine della posizione geografica.
+ * @param longitudine la longitudine della posizione geografica
+ */
     public void setLongitudine(double longitudine) {
         this.longitudine = longitudine;
     }
 
+/**
+ * Restituisce il luogo ricavato tramite geocoding dal server.
+ * @return il luogo ricavato tramite geocoding dal server
+ */
     public String getLuogo() {
         return luogo;
     }
 
+/**
+ * Imposta il luogo ricavato tramite geocoding dal server.
+ * @param luogo il luogo ricavato tramite geocoding dal server
+ */
     public void setLuogo(String luogo) {
         this.luogo = luogo;
     }
 
 //Metodo toString
+/**
+ * Restituisce una rappresentazione testuale dell'oggetto PosizioneDTO.
+ * @return la rappresentazione testuale dell'oggetto
+ */
     @Override
     public String toString() {
         return "PosizioneDTO [latitudine=" + latitudine + ", longitudine=" + longitudine + 
--- theknife-common/src/main/java/theknife/common/dto/RecensioneDTO.java
+++ theknife-common/src/main/java/theknife/common/dto/RecensioneDTO.java
@@ -8,18 +8,70 @@ import java.time.LocalDateTime;
  */
 
 public class RecensioneDTO implements java.io.Serializable {
+/**
+ * Costante di serializzazione per la compatibilità tra versioni della classe durante la serializzazione e 
+ * deserializzazione.
+ */
     private static final long serialVersionUID = 1L;
+
+/**
+ * L'identificatore univoco della recensione.
+ */
     private long idRecensione;
+    
+/**
+ * L'identificatore univoco del ristorante per cui è stata scritta la recensione.
+ */
     private long idRistorante;
+    
+/**
+ * Il titolo della recensione.
+ */
     private String titolo;
+    
+/**
+ * Il testo della recensione.
+ */
     private String testo;
+    
+/**
+ * Il numero di stelle assegnate alla recensione.
+ */
     private int stelle;
+    
+/**
+ * L'identificatore univoco dell'utente che ha scritto la recensione.
+ */
     private long idUtente;
+    
+/**
+ * La data e l'ora di pubblicazione della recensione.
+ */
     private LocalDateTime dataPubblicazione;
+    
+/**
+ * La risposta alla recensione.
+ */
     private String risposta;
+    
+/**
+ * La data e l'ora di pubblicazione della risposta alla recensione.
+ */
     private LocalDateTime dataRisposta;
 
 //Costruttore
+/**
+ * Crea il DTO per la recensione con i dati specificati.
+ * @param idRecensione l'identificatore della recensione
+ * @param idRistorante l'identificatore del ristorante
+ * @param titolo il titolo della recensione
+ * @param testo il testo della recensione
+ * @param stelle il numero di stelle della recensione
+ * @param idUtente l'idetificatore dell'utente
+ * @param dataPubblicazione la data e l'ora di pubblicazione della recensione
+ * @param risposta la risposta alla recensione
+ * @param dataRisposta la data e l'ora di pubblicazione della risposta alla recensione
+ */
     public RecensioneDTO(long idRecensione, long idRistorante, String titolo, String testo, 
             int stelle, long idUtente, LocalDateTime dataPubblicazione, String risposta, 
             LocalDateTime dataRisposta) {
@@ -35,79 +87,155 @@ public class RecensioneDTO implements java.io.Serializable {
     }
 
 //Getters e Setters
+/**
+ * Restituisce l'identificatore della recensione.
+ * @return l'identificatore della recensione
+ */
     public long getIdRecensione() {
         return idRecensione;
     }
 
+/**
+ * Imposta l'identificatore della recensione.
+ * @param idRecensione l'identificatore della recensione
+ */
     public void setIdRecensione(long idRecensione) {
         this.idRecensione = idRecensione;
     }
 
+/**
+ * Restituisce l'identificatore del ristorante per cui è stata scritta la recensione.
+ * @return l'identificatore del ristorante
+ */
     public long getIdRistorante() {
         return idRistorante;
     }
 
+/**
+ * Imposta l'identificatore del ristorante per cui è stata scritta la recensione.
+ * @param idRistorante l'identificatore del ristorante
+ */
     public void setIdRistorante(long idRistorante) {
         this.idRistorante = idRistorante;
     }
 
+/**
+ * Restituisce il titolo della recensione.
+ * @return il titolo della recensione
+ */
     public String getTitolo() {
         return titolo;
     }
 
+/**
+ * Imposta il titolo della recensione.
+ * @param titolo il titolo della recensione
+ */
     public void setTitolo(String titolo) {
         this.titolo = titolo;
     }
 
+/**
+ * Restituisce il testo della recensione.
+ * @return il testo della recensione
+ */
     public String getTesto() {
         return testo;
     }
 
+/**
+ * Imposta il testo della recensione.
+ * @param testo il testo della recensione
+ */
     public void setTesto(String testo) {
         this.testo = testo;
     }
 
+/**
+ * Restituisce il numero di stelle della recensione.
+ * @return il numero di stelle della recensione
+ */
     public int getStelle() {
         return stelle;
     }
 
+/**
+ * Imposta il numero di stelle della recensione.
+ * @param stelle il numero di stelle della recensione
+ */
     public void setStelle(int stelle) {
         this.stelle = stelle;
     }
 
+/**
+ * Restituisce l'identificatore dell'utente che ha scritto la recensione.
+ * @return l'identificatore dell'utente
+ */
     public long getIdUtente() {
         return idUtente;
     }
 
+/**
+ * Imposta l'identificatore dell'utente che ha scritto la recensione.
+ * @param idUtente l'identificatore dell'utente
+ */
     public void setIdUtente(long idUtente) {
         this.idUtente = idUtente;
     }
 
+/**
+ * Restituisce la data e l'ora di pubblicazione della recensione.
+ * @return la data e l'ora di pubblicazione della recensione
+ */
     public LocalDateTime getDataPubblicazione() {
         return dataPubblicazione;
     }
 
+/**
+ * Imposta la data e l'ora di pubblicazione della recensione.
+ * @param dataPubblicazione la data e l'ora di pubblicazione della recensione
+ */
     public void setDataPubblicazione(LocalDateTime dataPubblicazione) {
         this.dataPubblicazione = dataPubblicazione;
     }
 
+/**
+ * Restituisce la risposta alla recensione.
+ * @return la risposta alla recensione
+ */
     public String getRisposta() {
         return risposta;
     }
 
+/**
+ * Imposta la risposta alla recensione.
+ * @param risposta la risposta alla recensione
+ */
     public void setRisposta(String risposta) {
         this.risposta = risposta;
     }
 
+/**
+ * Restituisce la data e l'ora della risposta alla recensione.
+ * @return la data e l'ora della risposta alla recensione
+ */
     public LocalDateTime getDataRisposta() {
         return dataRisposta;
     }
 
+/**
+ * Imposta la data e l'ora della risposta alla recensione.
+ * @param dataRisposta la data e l'ora della risposta alla recensione
+ */
     public void setDataRisposta(LocalDateTime dataRisposta) {
         this.dataRisposta = dataRisposta;
     }
 
 //Metodo toString
+/**
+ * Restituisce una rappresentazione testuale dell'oggetto RecensioneDTO.
+ * @return la rappresentazione testuale dell'oggetto
+ */
     @Override
     public String toString() {
         return "RecensioneDTO [idRecensione=" + idRecensione + ", idRistorante=" + idRistorante + 
