d08612ac339855f79b438eee458d18c0400f598b
Gaspa999 <lgasparini@studenti.uninsubria.it>
2026-08-16 17:47:04 +0200

    Documentazione di IdRecensioneDTO e IdRistoranteDTO

--- theknife-common/src/main/java/theknife/common/dto/IdRecensioneDTO.java
+++ theknife-common/src/main/java/theknife/common/dto/IdRecensioneDTO.java
@@ -7,21 +7,49 @@ package theknife.common.dto;
  */
 
 public class IdRecensioneDTO implements java.io.Serializable {
+/**
+ * Costante di serializzazione per la compatibilità tra versioni della classe durante la serializzazione e
+ * deserializzazione.
+ */
     private static final long serialVersionUID = 1L;
+    
+/**
+ * L'ID della recensione.
+ */
     private long idRecensione;
+
 //Costruttore
+/**
+ * Crea il DTO per l'identificatore di una recensione con l'ID specificato.
+ * @param idRecensione l'ID della recensione
+ */
     public IdRecensioneDTO(long idRecensione) {
         this.idRecensione = idRecensione;
     }
+
 //Getter
+/**
+ * Restituisce l'ID della recensione.
+ * @return l'ID della recensione
+ */
     public long getIdRecensione() {
         return idRecensione;
     }
+
 //Setter
+/**
+ * Imposta l'ID della recensione.
+ * @param idRecensione l'ID della recensione
+ */
     public void setIdRecensione(long x) {
         this.idRecensione = x;
     }
+
 //Metodo toString
+/**
+ * Restituisce una rappresentazione testuale dell'oggetto IdRecensioneDTO.
+ * @return la rappresentazione testuale dell'oggetto
+ */
     @Override
     public String toString() {
         return "IdRecensioneDTO [idRecensione=" + idRecensione + "]";
--- theknife-common/src/main/java/theknife/common/dto/IdRistoranteDTO.java
+++ theknife-common/src/main/java/theknife/common/dto/IdRistoranteDTO.java
@@ -7,22 +7,48 @@ package theknife.common.dto;
  */
 
 public class IdRistoranteDTO implements java.io.Serializable {
+/**
+ * Costante di serializzazione per la compatibilità tra versioni della classe durante la serializzazione e
+ * deserializzazione.
+ */
     private static final long serialVersionUID = 1L;
+    
+/**
+ * L'ID del ristorante.
+ */
     private long idRistorante;
 
 //Costruttore
+/**
+ * Crea il DTO per l'identificatore di un ristorante con l'ID specificato.
+ * @param idRistorante l'ID del ristorante
+ */
     public IdRistoranteDTO(long idRistorante) {
         this.idRistorante = idRistorante;
     }
-//Getters e Setters
+
+//Getter e Setter
+/**
+ * Restituisce l'ID del ristorante.
+ * @return l'ID del ristorante
+ */
     public long getIdRistorante() {
         return idRistorante;
     }
 
+/**
+ * Imposta l'ID del ristorante.
+ * @param idRistorante l'ID del ristorante
+ */
     public void setIdRistorante(long idRistorante) {
         this.idRistorante = idRistorante;
     }
+
 //Metodo toString
+/**
+ * Restituisce una rappresentazione testuale dell'oggetto IdRistoranteDTO.
+ * @return la rappresentazione testuale dell'oggetto
+ */
     @Override
     public String toString() {
         return "IdRistoranteDTO [idRistorante=" + idRistorante + "]";
