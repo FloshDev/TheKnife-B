1555985d9fb424e3eaf1fb4f84d1d837574a0a14
Gaspa999 <lgasparini@studenti.uninsubria.it>
2026-08-17 21:31:42 +0200

    Conclusione della documentazione del package dto

--- theknife-common/src/main/java/theknife/common/dto/ServizioDTO.java
+++ theknife-common/src/main/java/theknife/common/dto/ServizioDTO.java
@@ -8,38 +8,70 @@ package theknife.common.dto;
 
 public class ServizioDTO implements java.io.Serializable {
 /**
- * 
+ * Costante di serializzazione per la compatibilità tra versioni della classe durante la serializzazione e 
+ * deserializzazione.
  */
     private static final long serialVersionUID = 1L;
     
+/**
+ * L'identificatore univoco del servizio.
+ */
     private long idServizio;
     
+/**
+ * Il nome del servizio.
+ */
     private String nomeServizio;
 
 //Costruttore
+/**
+ * Crea il DTO per il servizio con i dati specificati.
+ * @param idServizio l'identificatore del servizio.
+ * @param nomeServizio il nome del servizio.
+ */
     public ServizioDTO(long idServizio, String nomeServizio) {
         this.idServizio = idServizio;
         this.nomeServizio = nomeServizio;
     }
 
 //Getters e Setters
+/**
+ * Restituisce l'identificatore del servizio.
+ * @return l'identificatore del servizio
+ */
     public long getIdServizio() {
         return idServizio;
     }
 
+/**
+ * Imposta l'identificatore del servizio.
+ * @param idServizio l'identificatore del servizio
+ */
     public void setIdServizio(long idServizio) {
         this.idServizio = idServizio;
     }
 
+/**
+ * Restituisce il nome del servizio.
+ * @return il nome del servizio
+ */
     public String getNomeServizio() {
         return nomeServizio;
     }
 
+/**
+ * Imposta il nome del servizio.
+ * @param nomeServizio il nome del servizio
+ */
     public void setNomeServizio(String nomeServizio) {
         this.nomeServizio = nomeServizio;
     }
 
 //Metodo toString
+/**
+ * Restituisce una rappresentazione testuale dell'oggetto ServizioDTO.
+ * @return la rappresentazione testuale dell'oggetto
+ */
     @Override
     public String toString() {
         return "ServizioDTO [idServizio=" + idServizio + ", nomeServizio=" + nomeServizio + "]";
--- theknife-common/src/main/java/theknife/common/dto/StatisticheRistoranteDTO.java
+++ theknife-common/src/main/java/theknife/common/dto/StatisticheRistoranteDTO.java
@@ -7,13 +7,40 @@ package theknife.common.dto;
  */
 
 public class StatisticheRistoranteDTO implements java.io.Serializable {
+/**
+ * Costante di serializzazione per la compatibilità tra versioni della classe durante la serializzazione e 
+ * deserializzazione.
+ */
     private static final long serialVersionUID = 1L;
+    
+/**
+ * L'identificatore univoco del ristorante.
+ */
     private long idRistorante;
+    
+/**
+ * Il nome del ristorante.
+ */
     private String nomeRistorante;
+    
+/**
+ * La media delle stelle assegnate al ristorante.
+ */
     private double mediaStelle;
+    
+/**
+ * Il numero di recensioni del ristorante.
+ */
     private int numeroRecensioni;
 
 //Costruttore
+/**
+ * Crea il DTO per le statistiche del ristorante con i dati specificati.
+ * @param idRistorante l'identificatore del ristorante
+ * @param nomeRistorante il nome del ristorante
+ * @param mediaStelle la media delle stelle assegnate al ristorante
+ * @param numeroRecensioni il numero di recensioni del ristorante
+ */
     public StatisticheRistoranteDTO(long idRistorante, String nomeRistorante, double mediaStelle, 
             int numeroRecensioni) {
         this.idRistorante = idRistorante;
@@ -21,40 +48,77 @@ public class StatisticheRistoranteDTO implements java.io.Serializable {
         this.mediaStelle = mediaStelle;
         this.numeroRecensioni = numeroRecensioni;
     }
+
 //Getters e Setters
+/**
+ * Restituisce l'identificatore del ristorante.
+ * @return l'identificatore del ristorante
+ */
     public long getIdRistorante() {
         return idRistorante;
     }   
 
+/**
+ * Imposta l'identificatore del ristorante.
+ * @param idRistorante l'identificatore del ristorante
+ */
     public void setIdRistorante(long idRistorante) {
         this.idRistorante = idRistorante;
     }
 
+/**
+ * Restituisce il nome del ristorante.
+ * @return il nome del ristorante
+ */
     public String getNomeRistorante() {
         return nomeRistorante;
     }
 
+/**
+ * Imposta il nome del ristorante.
+ * @param nomeRistorante il nome del ristorante
+ */
     public void setNomeRistorante(String nomeRistorante) {
         this.nomeRistorante = nomeRistorante;
     }
 
+/**
+ * Restituisce la media delle stelle assegnate al ristorante.
+ * @return la media delle stelle assegnate al ristorante
+ */
     public double getMediaStelle() {
         return mediaStelle;
     }
 
+/**
+ * Imposta la media delle stelle assegnate al ristorante.
+ * @param mediaStelle la media delle stelle assegnate al ristorante
+ */
     public void setMediaStelle(double mediaStelle) {
         this.mediaStelle = mediaStelle;
     }
 
+/**
+ * Restituisce il numero di recensioni del ristorante.
+ * @return il numero di recensioni del ristorante
+ */
     public int getNumeroRecensioni() {
         return numeroRecensioni;
     }
 
+/**
+ * Imposta il numero di recensioni del ristorante.
+ * @param numeroRecensioni il numero di recensioni del ristorante
+ */
     public void setNumeroRecensioni(int numeroRecensioni) {
         this.numeroRecensioni = numeroRecensioni;
     }
 
 //Metodo toString
+/**
+ * Restituisce una rappresentazione testuale dell'oggetto StatisticheRistoranteDTO.
+ * @return la rappresentazione testuale dell'oggetto
+ */
     @Override
     public String toString() {
         return "StatisticheRistoranteDTO [idRistorante=" + idRistorante + 
